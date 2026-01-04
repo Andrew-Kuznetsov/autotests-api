cb0b6ae5d13479cfc017d2fc50ba6facdecbc1b3
Andrew-Kuznetsov <kuznetsov.andrew2308@yandex.ru>
2026-01-04 17:21:57 +0300

    Знакомство с API клиентами. Практика: Написание API клиента

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,27 @@
+from typing import TypedDict
+
+from httpx import Response
+from clients.api_client import ApiClient
+
+class UserCreateDict(TypedDict):
+    """
+    Описание структуры запроса на создание нового пользователя.
+    """
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+class PublicUsersClient(ApiClient):
+    """
+    Клиент для работы с /api/v1/users
+    """
+    def create_user_api(self, request: UserCreateDict) -> Response:
+        """
+        Метод на создание нового пользователя.
+
+        :param request: Словарь с email, password, lastName, firstName, middleName
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.post("/api/v1/users", json=request)
